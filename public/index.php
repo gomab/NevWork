aa7278690c37699a711f90e3f1a1e8de964b6b86
gomab <egoiomab@gamil.com>
2018-02-27 14:37:37 +0100

    Project Structure

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gomab
+ * Date: 2/27/18
+ * Time: 2:19 PM
+ */
+//Chargemement autoload composer
+require '../vendor/autoload.php';
+
+//Initialisation App()
+
+$app = new \Framework\App();
+
+$app->run();
---
+++ src/Framework/App.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gomab
+ * Date: 2/27/18
+ * Time: 2:29 PM
+ */
+
+namespace Framework;
+
+class App{
+
+    public function run(){
+        echo 'HI';
+    }
+}
